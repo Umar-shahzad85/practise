a3f75215dbc44a20389819598ca5a4aa36fdd0c1
umar shahzad <umarshahzadblouch85@gmail.com>
2025-03-25 10:28:49 +0500

    add form in script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const script = () => {
   return (
-    <div>script</div>
-  )
-}
+    <div>
+      <form>
+        <h4>Login</h4>
+        <div className="name">
+        <label htmlFor="name">Name:</label>
+        <input type="text" />
+        </div>
+        <div className="lname">
+        <label htmlFor="lname">Last Name:</label>
+        <input type="text" />
+        </div>
+        <div className="pass">
+        <label htmlFor="pass">passwords:</label>
+        <input type="paswords" />
+        </div>
+        <button>submited</button>
+      </form>
+    </div>
+  );
+};
 
-export default script
+export default script;
