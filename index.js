877951ed8b74185ad425183ace4a8d0f430a3fa4
umar shahzad <umarshahzadblouch85@gmail.com>
2025-03-25 16:16:48 +0500

    add new fub=n with the name of city

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 console.log("my name is umar")
 const name=farzeen
 const age = 23;
+const cit ='lahore'
 
 function Name(name){
     console.log("my name is ", name);
@@ -11,5 +12,10 @@ function Name(name){
 
 const ageer = (age)=>{
     console.log(`the ferzeen age is : ${age}`);
+    
+}
+
+const city = (cit)=>{
+    console.log(`the ferzeen age is : ${cit}`);
     
 }
