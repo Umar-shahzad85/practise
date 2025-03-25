b7b3c78526ab2688f73ff9245dcdb58a6f3723fd
umar shahzad <umarshahzadblouch85@gmail.com>
2025-03-25 15:02:15 +0500

    add new ager function

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+
+
 console.log("my name is umar")
 const name=farzeen
+const age = 23;
 
 function Name(name){
     console.log("my name is ", name);
-    
 }
 
+
+const ageer = (age)=>{
+    console.log(`the ferzeen age is : ${age}`);
+    
+}
