b80bef95a1838d8d0a07b0a68683982f17408e85
umar shahzad <umarshahzadblouch85@gmail.com>
2025-03-26 11:27:30 +0500

    adds new changings in in functions

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-
-
-console.log("my name is umar")
-const name=farzeen
+console.log("my name is umar");
+const name = farzeen;
 const age = 23;
-const cit ='lahore'
+const cit = "lahore";
 
-function Name(name){
-    console.log("my name is ", name);
+function Name(name) {
+  console.log("my name is ", name);
 }
 
+const ageer = (age) => {
+  console.log(`the ferzeen age is : ${age}`);
+};
 
-const ageer = (age)=>{
-    console.log(`the ferzeen age is : ${age}`);
-    
-}
+const city = (cit) => {
+  console.log(`the ferzeen age is : ${cit}`);
+};
 
-const city = (cit)=>{
-    console.log(`the ferzeen age is : ${cit}`);
-    
-}
+const Ba = (cit) => {
+  console.log(`the ferzeen age is : ${cit}`);
+};
+
+const csv = (cit) => {
+  console.log(`the ferzeen age is : ${cit}`);
+};
+
+const dcvs = (cit) => {
+  console.log(`the ferzeen age is : ${cit}`);
+};
